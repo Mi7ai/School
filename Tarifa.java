6b9b554085572e6043d29db162eaba68d1194ca2
Mi7ai <hottmayer@gmail.com>
2017-03-01 18:40:45 +0100

    Add files via upload

---
+++ Tarifa.java
@@ -0,0 +1,13 @@
+package facturacion;
+
+public class Tarifa {
+	private float tarifa;
+	
+	public Tarifa(float tarifa){
+		this.tarifa = tarifa;
+	}
+	
+	float getTarifa(){
+		return tarifa;
+	}
+}
