9139cb4006fcab03ea6b55d24140f45c2287d9a1
Mi7ai <hottmayer@gmail.com>
2017-03-07 14:25:48 +0100

    Add files via upload

---
+++ genericos/ListadoAPartirDeFechas.java
@@ -0,0 +1,33 @@
+package genericos;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import facturacion.Fecha;
+
+public class ListadoAPartirDeFechas implements Comparable<Fecha>{
+
+	public static Collection<? extends Fecha> listado(Collection<? extends Fecha> conjunto,LocalDateTime fechaInicio, LocalDateTime fechaFin){
+		Collection<Fecha> nuevoConjunto =  new ArrayList<>();
+		for ( tipo : nuevoConjunto) {//mostrar listado clientes entre 2 fechas
+			if () {
+				
+			}
+			
+		}
+		return conjunto;
+		
+		 
+		
+	}
+
+	@Override
+	public int compareTo(Fecha f) {
+		// TODO Auto-generated method stub
+		
+		 
+		return 0;
+	}
+}
