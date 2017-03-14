5e39444692004a40ee4965d415dce35984613307
Mi7ai <hottmayer@gmail.com>
2017-03-14 13:32:59 +0100

    Metodo completado

--- genericos/ListadoAPartirDeFechas.java
+++ genericos/ListadoAPartirDeFechas.java
@@ -7,27 +7,20 @@ import java.util.HashMap;
 
 import facturacion.Fecha;
 
-public class ListadoAPartirDeFechas implements Comparable<Fecha>{
+public class ListadoAPartirDeFechas {
 
 	public static Collection<? extends Fecha> listado(Collection<? extends Fecha> conjunto,LocalDateTime fechaInicio, LocalDateTime fechaFin){
-		Collection<Fecha> nuevoConjunto =  new ArrayList<>();
-		for ( tipo : nuevoConjunto) {//mostrar listado clientes entre 2 fechas
-			if () {
-				
-			}
-			
+		ArrayList<Fecha> nuevoConjunto =  new ArrayList<>();
+		for (Fecha f : nuevoConjunto) {//mostrar listado clientes entre 2 fechas
+			if (f.getFecha().compareTo(fechaInicio) > 0 && f.getFecha().compareTo(fechaFin) < 0) {
+				nuevoConjunto.add(f);
+			}			
 		}
-		return conjunto;
+		return nuevoConjunto;
 		
 		 
 		
+
 	}
 
-	@Override
-	public int compareTo(Fecha f) {
-		// TODO Auto-generated method stub
-		
-		 
-		return 0;
-	}
 }
