ee52daef03b0992f0fd65ea26f11d78f7913f0b9
mi7ai <hottmayer@gmail.com>
2017-05-01 18:22:35 +0200

    mcv

---
+++ vista/ImplementaVista.java
@@ -0,0 +1,12 @@
+package vista;
+
+import controlador.MetodosVista;
+
+public class ImplementaVista implements MetodosControlador{
+	MetodosVista controlador;
+	@Override
+	public void operacionClientes() {
+		
+	}
+
+}
